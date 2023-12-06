793141092cd10e743e10f2ab03dc7ae133e9cebb
PawelKarczewski <pawel.karczewski@wealtharc.com>
2023-12-07 00:09:55 +0100

    Porządki w kodzie

--- Lab1/Facebook/NumerOfUsersPrediction.py
+++ Lab1/Facebook/NumerOfUsersPrediction.py
@@ -15,41 +15,26 @@ model.fit(trainX, trainValues)
 
 print("Współczynnik determinacji: ", model.score(trainX, trainValues))
 
+def testModel(yearFrom, yearTo):
+    testData = allData[(yearFrom <= allData['Year']) & (allData['Year'] <= yearTo)].copy()
+
+    testX = pd.to_numeric(pd.to_datetime(testData['Date'])).values.reshape(-1, 1)
+
+    testData['GeneratedByModel'] = model.predict(testX)
+    testData['Error'] = testData['Users_in_mln'] - testData['GeneratedByModel']
+    testData['Error_squared'] = testData['Error'] ** 2
+
+    print(testData)
+    print()
+
 # Check model ---------------------------------------------------------------
 print("Testownie modelu 2016-2017:\n")
-testData = allData[(2016 <= allData['Year']) & (allData['Year'] < 2018)].copy()
-
-testX = pd.to_numeric(pd.to_datetime(testData['Date'])).values.reshape(-1, 1)
-
-testData['GeneratedByModel'] = model.predict(testX)
-testData['Error'] = testData['Users_in_mln'] - testData['GeneratedByModel']
-testData['Error_squared'] = testData['Error'] ** 2
-
-print(testData)
-print()
+testModel(2016, 2017)
 
 # Predict 2018-2020 ---------------------------------------------------------
 print("Predykcja 2018-2020:\n")
-predictData = allData[(2018 <= allData['Year']) & (allData['Year'] < 2021)].copy()
-
-predictX = pd.to_numeric(pd.to_datetime(predictData['Date'])).values.reshape(-1, 1)
-
-predictData['GeneratedByModel'] = model.predict(predictX)
-predictData['Error'] = predictData['Users_in_mln'] - predictData['GeneratedByModel']
-predictData['Error_squared'] = predictData['Error'] ** 2
-
-print(predictData)
-print()
+testModel(2018, 2020)
 
 # Predict 2021-2022 ---------------------------------------------------------
 print("Predykcja 2021-2022:\n");
-predictData = allData[(2021 <= allData['Year']) & (allData['Year'] < 2023)].copy()
-
-predictX = pd.to_numeric(pd.to_datetime(predictData['Date'])).values.reshape(-1, 1)
-
-predictData['GeneratedByModel'] = model.predict(predictX)
-predictData['Error'] = predictData['Users_in_mln'] - predictData['GeneratedByModel']
-predictData['Error_squared'] = predictData['Error'] ** 2
-
-print(predictData)
-print()
+testModel(2021, 2022)
