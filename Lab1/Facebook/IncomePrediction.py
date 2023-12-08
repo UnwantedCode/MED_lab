e9a4d18a20d0fc437334bcba664a3877bfbd8a61
PawelKarczewski <pawel.karczewski@wealtharc.com>
2023-12-08 20:17:48 +0100

    make it working

--- Lab1/Facebook/IncomePrediction.py
+++ Lab1/Facebook/IncomePrediction.py
@@ -36,7 +36,6 @@ def TrainModels(yearFrom, yearTo, variableFrom, variableTo):
 
     trainX = trainData[independentVariableName].values
     trainValues = trainData[dependentVariableName].values
-    fit = np.polyfit(trainX, np.log(trainValues), 1)
     
     model = sm.OLS(trainValues, sm.add_constant(trainX)).fit()
     degreeOfFreedom1 = model.df_model # liczba zmiennych objaśniających
@@ -46,7 +45,7 @@ def TrainModels(yearFrom, yearTo, variableFrom, variableTo):
     print(f'Wartość krytyczna dla alpha {alpha}: {critical_value}')
 
     exponentialModel = LinearRegression()
-    xForExponential = trainX.reshape(-1, 1)
+    xForExponential = sm.add_constant(trainX.reshape(-1, 1))
     yForExponential = np.log(trainValues).reshape(-1, 1)
     exponentialModel.fit(xForExponential, yForExponential) # ln(y) = ln(a)x + ln(b) => y = a^x * b wykładniczy
     print("Model wykładniczy y = a^x * b zlinearyzowny do: ln(y) = ln(a)x + ln(b)")
@@ -55,14 +54,14 @@ def TrainModels(yearFrom, yearTo, variableFrom, variableTo):
     exponentialScore = exponentialModel.score(xForExponential, yForExponential)
     FValue = (exponentialScore / (1 - exponentialScore)) * ((len(trainX) - 2) / 1)
     print("Wartość F dla modelu wykładniczego: ", FValue)
-    exponentialModel_statsmodels = sm.OLS(yForExponential, sm.add_constant(xForExponential)).fit()
+    exponentialModel_statsmodels = sm.OLS(yForExponential, xForExponential).fit()
     print("Wartość F dla modelu wykładniczego z biblioteki statsmodel: ", exponentialModel_statsmodels.fvalue)
     print("Wartość p-value dla modelu wykładniczego z biblioteki statsmodel: ", exponentialModel_statsmodels.f_pvalue)
 
     print()
 
     powerModel = LinearRegression()
-    xForPower = np.log(trainX).reshape(-1, 1)
+    xForPower = sm.add_constant(np.log(trainX).reshape(-1, 1))
     yForPower = np.log(trainValues).reshape(-1, 1)
     powerModel.fit(xForPower, yForPower) # ln(y) = a*ln(x) + ln(b) => y = x^a * b potęgowy
     print("Model potęgowy y = x^a * b zlinearyzowny do: ln(y) = a*ln(x) + ln(b)")
@@ -71,7 +70,7 @@ def TrainModels(yearFrom, yearTo, variableFrom, variableTo):
     powerScore = powerModel.score(xForPower, yForPower)
     FValue = (powerScore / (1 - powerScore)) * ((len(trainX) - 2) / 1)
     print("Wartość F dla modelu potęgowego: ", FValue)
-    powerModel_statsmodels = sm.OLS(yForPower, sm.add_constant(xForPower)).fit()
+    powerModel_statsmodels = sm.OLS(yForPower, xForPower).fit()
     print("Wartość F dla modelu potęgowego z biblioteki statsmodel: ", powerModel_statsmodels.fvalue)
     print("Wartość p-value dla modelu potęgowego z biblioteki statsmodel: ", powerModel_statsmodels.f_pvalue)
     print()
@@ -81,7 +80,7 @@ def TrainModels(yearFrom, yearTo, variableFrom, variableTo):
     x2 = np.power(trainX, 2).reshape(-1, 1)
     x3 = np.power(trainX, 3).reshape(-1, 1)
     x4 = np.power(trainX, 4).reshape(-1, 1)
-    xForPolynomialModel = np.concatenate((x1, x2, x3, x4), axis=1)
+    xForPolynomialModel = sm.add_constant(np.concatenate((x1, x2, x3, x4), axis=1))
     yForPolynomialModel = trainValues.reshape(-1, 1)
     polynomialModel.fit(xForPolynomialModel, yForPolynomialModel) # y = ax^4 + bx^3 + cx^2 + dx + e wielomianowy
     print("Model wielomianowy y = ax^4 + bx^3 + cx^2 + dx + e")
@@ -91,7 +90,7 @@ def TrainModels(yearFrom, yearTo, variableFrom, variableTo):
     polynomialScore = polynomialModel.score(xForPolynomialModel, yForPolynomialModel)
     FValue = (polynomialScore / (1 - polynomialScore)) * ((len(trainX) - 2) / 1)
     print("Wartość F dla modelu wielomianowego: ", FValue)
-    polynomialModel_statsmodels = sm.OLS(yForPolynomialModel, sm.add_constant(xForPolynomialModel)).fit()
+    polynomialModel_statsmodels = sm.OLS(yForPolynomialModel, xForPolynomialModel).fit()
     print("Wartość F dla modelu wielomianowego z biblioteki statsmodel: ", polynomialModel_statsmodels.fvalue)
     print("Wartość p-value dla modelu wielomianowego z biblioteki statsmodel: ", polynomialModel_statsmodels.f_pvalue)
     print()
@@ -99,7 +98,7 @@ def TrainModels(yearFrom, yearTo, variableFrom, variableTo):
     squareModel = LinearRegression()
     x1 = trainX.reshape(-1, 1)
     x2 = np.power(trainX, 2).reshape(-1, 1)
-    xForSquareModel = np.concatenate((x1, x2), axis=1)
+    xForSquareModel = sm.add_constant(np.concatenate((x1, x2), axis=1))
     yForSquareModel = trainValues.reshape(-1, 1)
     squareModel.fit(xForSquareModel, yForSquareModel) # y = ax^2 + bx + c kwadratowy
     print("Model kwadratowy y = ax^2 + bx + c")
@@ -109,13 +108,13 @@ def TrainModels(yearFrom, yearTo, variableFrom, variableTo):
     squareScore = squareModel.score(xForSquareModel, yForSquareModel)
     FValue = (squareScore / (1 - squareScore)) * ((len(trainX) - 2) / 1)
     print("Wartość F dla modelu kwadratowego: ", FValue)
-    squareModel_statsmodels = sm.OLS(yForSquareModel, sm.add_constant(xForSquareModel)).fit()
+    squareModel_statsmodels = sm.OLS(yForSquareModel, xForSquareModel).fit()
     print("Wartość F dla modelu kwadratowego z biblioteki statsmodel: ", squareModel_statsmodels.fvalue)
     print("Wartość p-value dla modelu kwadratowego z biblioteki statsmodel: ", squareModel_statsmodels.f_pvalue)
     print()
 
     linearModel = LinearRegression()
-    xForLinear = trainX.reshape(-1, 1)
+    xForLinear = sm.add_constant(trainX.reshape(-1, 1))
     yForLinear = trainValues.reshape(-1, 1)
     linearModel.fit(xForLinear, yForLinear) # y = ax + b liniowy
     print("Model liniowy y = ax + b")
@@ -125,11 +124,12 @@ def TrainModels(yearFrom, yearTo, variableFrom, variableTo):
     linearScore = linearModel.score(xForLinear, yForLinear)
     FValue = (linearScore / (1 - linearScore)) * ((len(trainX) - 2) / 1)
     print("Wartość F dla modelu liniowego: ", FValue)
-    linearModel_statsmodels = sm.OLS(yForLinear, sm.add_constant(xForLinear)).fit()
+    linearModel_statsmodels = sm.OLS(yForLinear, xForLinear).fit()
     print("Wartość F dla modelu liniowego z biblioteki statsmodel: ", linearModel_statsmodels.fvalue)
     print("Wartość p-value dla modelu liniowego z biblioteki statsmodel: ", linearModel_statsmodels.f_pvalue)
     print()
 
+    fit = np.polyfit(trainX, np.log(trainValues), 1)
     trainData['Wykładniczy ręczny'] = np.exp(trainX * fit[0] + fit[1])
     trainData['Wykładniczy'] = np.exp(exponentialModel.predict(xForExponential))
     trainData['Potęgowy'] = np.exp(powerModel.predict(xForPower))
@@ -143,48 +143,48 @@ def TrainModels(yearFrom, yearTo, variableFrom, variableTo):
 def TestModels(yearFrom, yearTo):
     print("Testowanie modelu (wyniki błędu dla różnych modeli) dla lat" , yearFrom , "-", yearTo, "...\n")
     testData = allData[(yearFrom <= allData['Year']) & (allData['Year'] <= yearTo)].copy()
-    testX = testData[independentVariableName].values
+    testX = sm.add_constant(testData[independentVariableName].values.reshape(-1, 1))
     meanSquareErrorsForModels = []
 
-    testData['Wykładniczy ręczny'] = np.exp(testX * fit[0] + fit[1])
+    testData['Wykładniczy ręczny'] = np.exp(testData[independentVariableName].values * fit[0] + fit[1])
     testData['Wykładniczy ręczny błąd'] = testData['Wykładniczy ręczny'] - testData[dependentVariableName]
     meanSquareError = np.mean(testData['Wykładniczy ręczny błąd'] ** 2)
     print("Średni błąd kwadratowy dla modelu wykładniczego ręcznego: ", meanSquareError)
     meanSquareErrorsForModels.append(("wykładniczy ręczny",meanSquareError))
 
-    testData['Wykładniczy'] = np.exp(exponentialModel.predict(testX.reshape(-1, 1)))
+    testData['Wykładniczy'] = np.exp(exponentialModel.predict(testX))
     testData['Wykładniczy błąd'] = testData['Wykładniczy'] - testData[dependentVariableName]
     meanSquareError = np.mean(testData['Wykładniczy błąd'] ** 2)
     print("Średni błąd kwadratowy dla modelu wykładniczego: ", meanSquareError)
     meanSquareErrorsForModels.append(("wykładniczy",meanSquareError))
 
-    testData['Potęgowy'] = np.exp(powerModel.predict(np.log(testX).reshape(-1, 1)))
+    testData['Potęgowy'] = np.exp(powerModel.predict(np.log(testX)))
     testData['Potęgowy błąd'] = testData['Potęgowy'] - testData[dependentVariableName]
     meanSquareError = np.mean(testData['Potęgowy błąd'] ** 2)
     print("Średni błąd kwadratowy dla modelu potęgowego: ", meanSquareError)
     meanSquareErrorsForModels.append(("potęgowy",meanSquareError))
 
-    xt1 = testX.reshape(-1, 1)
-    xt2 = np.power(testX, 2).reshape(-1, 1)
-    xt3 = np.power(testX, 3).reshape(-1, 1)
-    xt4 = np.power(testX, 4).reshape(-1, 1)
-    X2 = np.concatenate((xt1, xt2, xt3, xt4), axis=1)
+    xt1 = testData[independentVariableName].values.reshape(-1, 1)
+    xt2 = np.power(xt1, 2).reshape(-1, 1)
+    xt3 = np.power(xt1, 3).reshape(-1, 1)
+    xt4 = np.power(xt1, 4).reshape(-1, 1)
+    X2 = sm.add_constant(np.concatenate((xt1, xt2, xt3, xt4), axis=1))
     testData['Wielomianowy'] = polynomialModel.predict(X2)
     testData['Wielomianowy błąd'] = testData['Wielomianowy'] - testData[dependentVariableName]
     meanSquareError = np.mean(testData['Wielomianowy błąd'] ** 2)
     print("Średni błąd kwadratowy dla modelu wielomianowego: ", meanSquareError)
     meanSquareErrorsForModels.append(("wielomianowy",meanSquareError))
 
-    xt1 = testX.reshape(-1, 1)
-    xt2 = np.power(testX, 2).reshape(-1, 1)
-    X2 = np.concatenate((xt1, xt2), axis=1)
+    xt1 = testData[independentVariableName].values.reshape(-1, 1)
+    xt2 = np.power(xt1, 2).reshape(-1, 1)
+    X2 = sm.add_constant(np.concatenate((xt1, xt2), axis=1))
     testData['Kwadratowy'] = squareModel.predict(X2)
     testData['Kwadratowy błąd'] = testData['Kwadratowy'] - testData[dependentVariableName]
     meanSquareError = np.mean(testData['Kwadratowy błąd'] ** 2)
     print("Średni błąd kwadratowy dla modelu kwadratowego: ", meanSquareError)
     meanSquareErrorsForModels.append(("kwadratowy",meanSquareError))
 
-    testData['Liniowy'] = linearModel.predict(testX.reshape(-1, 1))
+    testData['Liniowy'] = linearModel.predict(testX)
     testData['Liniowy błąd'] = testData['Liniowy'] - testData[dependentVariableName]
     meanSquareError = np.mean(testData['Liniowy błąd'] ** 2)
     print("Średni błąd kwadratowy dla modelu liniowego: ", meanSquareError)
