ce11cc66439d8c2d00c7919cf7b4fa87e28363aa
PawelKarczewski <pawel.karczewski@wealtharc.com>
2024-01-17 14:23:29 +0100

    Dodano sprawozdanie do Lab3 zad1

--- Lab3/Matches/CalculationsBayes.py
+++ Lab3/Matches/CalculationsBayes.py
@@ -3,15 +3,15 @@ import pandas as pd
 print()
 
 allData = pd.read_csv('./Lab3/Matches/Data.csv')
-print('Wszystkie dane:')
-print(allData)
-print()
+# print('Wszystkie dane:')
+# print(allData)
+# print()
 
 # using naive bayes theorem
 
 matchPlayedCounts = allData['ZagranoMecz'].value_counts()
-print(matchPlayedCounts)
-print()
+# print(matchPlayedCounts)
+# print()
 
 probabilityOfPlayMatch = matchPlayedCounts['tak'] / allData['ZagranoMecz'].count()
 probabilityOfNotPlayMatch = matchPlayedCounts['nie'] / allData['ZagranoMecz'].count()
@@ -20,64 +20,64 @@ print('Prawdopodobienstwo nie zagrania meczu:', probabilityOfNotPlayMatch)
 print()
 
 playedMatch = allData[allData['ZagranoMecz'] == 'tak']
-print('Rozegrane mecze:')
-print(playedMatch)
-print()
+# print('Rozegrane mecze:')
+# print(playedMatch)
+# print()
 
 playedMatchForWindCount = playedMatch['SilaWiatru'].value_counts()
-print('Rozegrane mecze wg sily wiatru:')
-print(playedMatchForWindCount)
-print()
+# print('Rozegrane mecze wg sily wiatru:')
+# print(playedMatchForWindCount)
+# print()
 
 probabilityOfStrongWindWhileMatch = playedMatchForWindCount['silny'] / playedMatch['SilaWiatru'].count()
 print('Prawdopodobienstwo silnego wiatru podczas meczu:', probabilityOfStrongWindWhileMatch)
 print()
 
 playedMatchForCloudinessCount = playedMatch['Zachmurzenie'].value_counts()
-print('Rozegrane mecze wg zachmurzenia:')
-print(playedMatchForCloudinessCount)
-print()
+# print('Rozegrane mecze wg zachmurzenia:')
+# print(playedMatchForCloudinessCount)
+# print()
 
 probabilityOfSunnyWhileMatch = playedMatchForCloudinessCount['słonecznie'] / playedMatch['Zachmurzenie'].count()
 print('Prawdopodobienstwo slonecznej pogody podczas meczu:', probabilityOfSunnyWhileMatch)
 print()
 
 playedMatchForTemperatureCount = playedMatch['OdczuwalnaTemperatura'].value_counts()
-print('Rozegrane mecze wg odczuwalnej temperatury:')
-print(playedMatchForTemperatureCount)
-print()
+# print('Rozegrane mecze wg odczuwalnej temperatury:')
+# print(playedMatchForTemperatureCount)
+# print()
 
 probabilityOfWarmWhileMatch = playedMatchForTemperatureCount['ciepło'] / playedMatch['OdczuwalnaTemperatura'].count()
 print('Prawdopodobienstwo cieplej pogody podczas meczu:', probabilityOfWarmWhileMatch)
 print()
 
 notPlayedMatch = allData[allData['ZagranoMecz'] == 'nie']
-print('Nie rozegrane mecze:')
-print(notPlayedMatch)
-print()
+# print('Nie rozegrane mecze:')
+# print(notPlayedMatch)
+# print()
 
 notPlayedMatchForWindCount = notPlayedMatch['SilaWiatru'].value_counts()
-print('Nie rozegrane mecze wg sily wiatru:')
-print(notPlayedMatchForWindCount)
-print()
+# print('Nie rozegrane mecze wg sily wiatru:')
+# print(notPlayedMatchForWindCount)
+# print()
 
 probabilityOfStrongWindWhileNotMatch = notPlayedMatchForWindCount['silny'] / notPlayedMatch['SilaWiatru'].count()
 print('Prawdopodobienstwo silnego wiatru podczas nie rozegranego meczu:', probabilityOfStrongWindWhileNotMatch)
 print()
 
 notPlayedMatchForCloudinessCount = notPlayedMatch['Zachmurzenie'].value_counts()
-print('Nie rozegrane mecze wg zachmurzenia:')
-print(notPlayedMatchForCloudinessCount)
-print()
+# print('Nie rozegrane mecze wg zachmurzenia:')
+# print(notPlayedMatchForCloudinessCount)
+# print()
 
 probabilityOfSunnyWhileNotMatch = notPlayedMatchForCloudinessCount['słonecznie'] / notPlayedMatch['Zachmurzenie'].count()
 print('Prawdopodobienstwo slonecznej pogody podczas nie rozegranego meczu:', probabilityOfSunnyWhileNotMatch)
 print()
 
 notPlayedMatchForTemperatureCount = notPlayedMatch['OdczuwalnaTemperatura'].value_counts()
-print('Nie rozegrane mecze wg odczuwalnej temperatury:')
-print(notPlayedMatchForTemperatureCount)
-print()
+# print('Nie rozegrane mecze wg odczuwalnej temperatury:')
+# print(notPlayedMatchForTemperatureCount)
+# print()
 
 probabilityOfWarmWhileNotMatch = notPlayedMatchForTemperatureCount['ciepło'] / notPlayedMatch['OdczuwalnaTemperatura'].count()
 print('Prawdopodobienstwo cieplej pogody podczas nie rozegranego meczu:', probabilityOfWarmWhileNotMatch)
