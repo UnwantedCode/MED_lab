d9b91aa37d4a6c6e19fef3c7748576a6df219c89
PawelKarczewski <pawel.karczewski@wealtharc.com>
2024-01-18 13:55:42 +0100

    Dodane sprawozdanie z laboratoriów nr 2 zad 3

--- Lab2/OS/Calculations.py
+++ Lab2/OS/Calculations.py
@@ -1,55 +1,44 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression, LogisticRegression
-from scipy.optimize import curve_fit
+from sklearn.linear_model import LinearRegression
 
 allData = pd.read_csv('./Lab2/OS/Data.csv')
 allData['Cumulated_number_of_errors'] = allData['Number_of_errors'].cumsum()
-allData['Cumulated_number_of_errors_normalized'] = allData['Cumulated_number_of_errors'] / allData['Cumulated_number_of_errors'].max()
-
+print('Dane')
 print(allData)
+print()
 
-x_train, x_test, y_train, y_test = train_test_split(allData['Month_number'].values.reshape(-1, 1), allData['Cumulated_number_of_errors_normalized'].values.reshape(-1, 1), test_size=0.2, random_state=0)
+x_train, x_test, y_train, y_test = train_test_split(allData['Month_number'].values.reshape(-1, 1), allData['Cumulated_number_of_errors'].values.reshape(-1, 1), test_size=0.2, random_state=0)
 
 linearModel = LinearRegression()
 linearModel.fit(x_train, y_train)
 
-logisticModel = LogisticRegression()
-# logisticModel.fit(x_train, y_train)
+scale = y_train.max() + 1
+logitModel = LinearRegression()
+logitModel.fit(x_train, np.log(y_train/(scale - y_train)))
 
-def logistic_function(x, a, b):
-    return 1 / (1 + np.exp(-(a * x + b)))
-
-params, covariance = curve_fit(logistic_function, allData['Month_number'].values, allData['Cumulated_number_of_errors_normalized'].values)
-
-print(params)
-print(logistic_function(allData['Month_number'], *params))
-
-def ShowNumberOfErrors():
-    plt.plot(allData['Month_number'], allData['Number_of_errors'], c='b')
-
-    plt.xlabel('Nr miesiąca')
-    plt.ylabel('Ilość błędów')
-    plt.title('Wykaz ilości błędów w zależności od miesiąca')
-    plt.grid(True)
-    plt.legend(['Ilość błędów'])
-
-    plt.show()
 
 def ShowCumulatedNumberOfErrors():
-    plt.plot(allData['Month_number'], allData['Cumulated_number_of_errors_normalized'], c='r')
-    # plt.plot(allData['Month_number'], logisticModel.predict(allData['Month_number'].values.reshape(-1, 1)), c='g')
+    plt.plot(allData['Month_number'], allData['Cumulated_number_of_errors'], c='r')
     plt.plot(allData['Month_number'], linearModel.predict(allData['Month_number'].values.reshape(-1, 1)), c='b')
-    plt.plot(allData['Month_number'], logistic_function(allData['Month_number'], *params), c='y')
+    plt.plot(allData['Month_number'], scale / (1 + np.exp(-logitModel.predict(allData['Month_number'].values.reshape(-1, 1)))), c='g')
 
     plt.xlabel('Nr miesiąca')
     plt.ylabel('Ilość błędów')
-    plt.title('Wykaz ilości błędów w zależności od miesiąca')
+    plt.title('Zsumowana ilości błędów w zależności od miesiąca')
     plt.grid(True)
-    plt.legend(['Ilość błędów', 'Ilość błędów (regresja logistyczna)', 'Ilość błędów (regresja liniowa)', 'Ilość błędów (regresja logistyczna z krzywą)'])
+    plt.legend(['Ilość błędów', 'Ilość błędów (regresja liniowa)', 'Ilość błędów (regresja logistyczna)'])
 
     plt.show()
 
 ShowCumulatedNumberOfErrors()
+
+meanSquaredErrorForLinear = np.mean((linearModel.predict(x_test) - y_test) ** 2)
+meanSquaredErrorForLogit = np.mean((scale / (1 + np.exp(-logitModel.predict(x_test))) - y_test) ** 2)
+
+print('Błąd średniokwadratowy dla regresji liniowej: ', meanSquaredErrorForLinear)
+print('Błąd średniokwadratowy dla regresji logistycznej: ', meanSquaredErrorForLogit)
+
